731e25650683fb230ab809c7f0b809f08fac88e3
LAALAD <160270603+LAALAD@users.noreply.github.com>
2025-02-13 17:13:06 +0100

    base

---
+++ Parte1/practica_7_2/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.practica_7_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.practica_7_2 to javafx.fxml;
+    exports com.example.practica_7_2;
+}
